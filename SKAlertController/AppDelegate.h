1d3e7427dc23c1b4b23cbd10f5f1b0a9883d6bad
Wei <yuchen@17shihui.com>
2016-12-21 16:07:27 +0800

    空工程

---
+++ SKAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKAlertController
+//
+//  Created by mac on 16/12/21.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKAlertController
+//
+//  Created by mac on 16/12/21.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
