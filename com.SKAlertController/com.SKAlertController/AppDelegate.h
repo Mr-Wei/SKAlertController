9ac401c7d5803e01b9efe48a9c182d4a9c460bfd
Mr-Wei <weiyuchen1109@163.com>
2016-12-21 15:45:48 +0800

    空工程

---
+++ com.SKAlertController/com.SKAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  com.SKAlertController
+//
+//  Created by mac on 16/12/21.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ com.SKAlertController/com.SKAlertController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  com.SKAlertController
+//
+//  Created by mac on 16/12/21.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
